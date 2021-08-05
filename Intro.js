d9019fb466918c7652efe65e39cf965b1afd409c
park <wnd2005@gmail.com>
2021-08-05 20:36:17 +0900

    (*) Test Code

---
+++ Intro.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
+import styled from 'styled-components';
+
+class Intro extends Component {
+  render() {
+    return <div>Blank Page</div>
+  }
+}
+
+export default Intro;
